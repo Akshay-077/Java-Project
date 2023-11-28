7ab46fdddb8517f507fbd3901f6702cddde69997
Akshay <152203835+Akshay-077@users.noreply.github.com>
2023-11-28 14:55:44 +0000

    Add files via upload

---
+++ Print_OddEven_1To100.java
@@ -0,0 +1,23 @@
+package SimpleOddEven;
+
+public class Print_OddEven_1To100 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i <=100; i++) 
+		{
+			if(i%2==0) 
+			{
+				System.out.println(i+ " No is Even");	
+			}
+			
+			else
+			{
+				System.out.println(i+ " No is Odd");
+			}
+	
+		}
+
+	}
+}
