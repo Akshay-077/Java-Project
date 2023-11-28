1c8fae661dcdb518c485ce154d259989a4f8a241
Akshay <152203835+Akshay-077@users.noreply.github.com>
2023-11-28 12:30:50 +0000

    Add files via upload

---
+++ Rbi.java
@@ -0,0 +1,12 @@
+package Service.details;
+
+public interface Rbi {
+
+	
+	public void createAccount();
+	public void displayAllDetails();
+	public void depositeMoney();
+	public void withdrawal();
+	public void balanceCheck();
+	public void Updateinformation();
+}
